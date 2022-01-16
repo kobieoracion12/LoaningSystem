47e1343bc40cc6a2dd4457e5f5fada365118de4c
artleywin <nikko.rosano08@gmail.com>
2022-01-16 20:17:04 +0800

    register

---
+++ php/register.php
@@ -0,0 +1,94 @@
+<?php
+
+require_once("database.php");
+
+// Check button is NOT is clicked
+if(!isset($_POST['submit'])) {
+	header('location: sign-up.php');
+	exit();
+}
+
+// Go on
+$fname = $_POST['first_name'];
+$lname = $_POST['last_name'];
+$email = $_POST['email'];
+$mobile_no = $_POST['mobile_no'];
+$bday = $_POST['bday'];
+$age = $_POST['age'];
+$address = $_POST['address'];
+$username = $_POST['username'];
+$password = $_POST['password'];
+$cpassword = $_POST['confirm_password'];
+$checkbox = $_POST['agree'];
+$fileUploadName = $_FILES["identification"]["name"];
+
+if(    empty($fname) || empty($lname) || empty($email) || empty($mobile_no) || empty($bday) || empty($age) || empty($address) || empty($username) || empty($password) || empty($cpassword) || empty($fileUploadName))  {
+	header('location: sign-up.php?error=emptyfields');
+	exit();
+}
+
+// Check terms button is NOT is clicked
+if(!isset($_POST['agree'])) {
+	header('location: sign-up.php?error=terms');
+	exit();
+}
+
+if($password != $cpassword) {
+	header('location: sign-up.php?error=notmatch');
+	exit();
+}
+
+$target_dir = "uploads/";
+$target_file = $target_dir . basename($fileUploadName);
+$uploadOk = 1;
+$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+
+$check = getimagesize($_FILES["identification"]["tmp_name"]);
+if($check !== false) {
+  echo "File is an image - " . $check["mime"] . ".";
+  $uploadOk = 1;
+} else {
+  echo "File is not an image.";
+  $uploadOk = 0;
+}
+
+// Allow certain file formats
+if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+&& $imageFileType != "gif" ) {
+	header('location: sign-up.php?error=filetype');
+	exit();
+  $uploadOk = 0;
+}
+
+$tmp_name = $_FILES["identification"]["tmp_name"];
+
+// Check if $uploadOk is set to 0 by an error
+if ($uploadOk == 0) {
+	echo "Sorry, your file was not uploaded.";
+  // if everything is ok, try to upload file
+  } else {
+	if (move_uploaded_file($tmp_name, $target_file)) {
+	  echo "The file ". htmlspecialchars( basename($fileUploadName)). " has been uploaded.";
+	} else {
+	  echo "Sorry, there was an error uploading your file.";
+	}
+}
+
+$file = "uploads/".$fileUploadName;
+
+$sql = "INSERT INTO accounts (first_name, last_name, email_add, mobile_no, birth_date, age, address, valid_id, username, password) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+
+$stmt = mysqli_stmt_init($config);
+
+if (!mysqli_stmt_prepare($stmt,$sql))
+{
+	header('location: sign-up.php?error=servererror');
+	exit();
+}
+else
+{
+	mysqli_stmt_bind_param($stmt, "sssisissss", $fname, $lname, $email, $mobile_no, $bday, $age, $address, $file, $username, $password);
+	mysqli_stmt_execute($stmt);
+	header('location: sign-up.php?msg=success');
+	exit();
+}
--- php/sign-up.php
+++ php/sign-up.php
@@ -23,27 +23,72 @@
           	<div class="row g-0">
           		<div class="col g-0">
           			<h4><b>Registration Progress</b></h4><br>
+
+<!--
           			<div class="progress" style="height: 30px">
           				<div class="progress-bar w-50" role="progresbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100">50%</div>
           			</div>
+-->
           		</div>
           	</div>
 
-	        <form style="padding: 5%" method="post" action="registration.php">
+			  <?php
+			if(isset($_GET['error'])) {
+
+				if($_GET['error'] == "notmatch") {
+					echo '<div class="alert alert-danger" role="alert">
+					Password does not match.
+				  </div>';
+				}
+
+				if($_GET['error'] == "emptyfields") {
+					echo '<div class="alert alert-danger" role="alert">
+					All fields are required.
+				  </div>';
+				}
+
+				if($_GET['error'] == "terms") {
+					echo '<div class="alert alert-danger" role="alert">
+					You did not agree to the Terms & Conditions.
+				  </div>';
+				}
+
+				if($_GET['error'] == "filetype") {
+					echo '<div class="alert alert-danger" role="alert">
+					File type cannot be read.
+				  </div>';
+				}
+
+
+			}
+
+			if(isset($_GET['msg'])) {
+
+				if($_GET['msg'] == "success") {
+					echo '<div class="alert alert-success" role="alert">
+					Registration successful!
+			 	 </div>';
+				}
+
+			}
+
+			?>
+
+<form style="padding: 5%" method="POST" action="register.php" enctype="multipart/form-data">
 	            <div class="row g-0">
 	            	<div class="col g-0" style="padding: 2%">
 	            		<div class="container-fluid">
 	            			<label for="first-input" class="form-label" style="float:left; font-weight: bold; font-size: 1.1vw">First Name</label><br>
-	                      	<input type="text" id="first-input" class="form-control form-style" placeholder="First Name"><br>
+	                      	<input type="text" id="first-input" name="first_name" class="form-control form-style" placeholder="First Name"><br>
 
 	                      	<label for="last-input" class="form-label" style="float:left; font-weight: bold; font-size: 1.1vw">Last Name</label><br>
-	                      	<input type="text" id="last-input" class="form-control form-style" placeholder="Last Name"><br>
+	                      	<input type="text" id="last-input" name="last_name" class="form-control form-style" placeholder="Last Name"><br>
 
 	                      	<label for="email-input" class="form-label" style="float:left; font-weight: bold; font-size: 1.1vw">Email Address</label><br>
-	                      	<input type="email" id="email-input" class="form-control form-style" placeholder="example@email.com"><br>
+	                      	<input type="email" id="email-input" name="email" class="form-control form-style" placeholder="example@email.com"><br>
 
 	                      	<label for="number-input" class="form-label" style="float:left; font-weight: bold; font-size: 1.1vw">Mobile Number</label><br>
-	                      	<input type="number" id="number-input" class="form-control form-style" placeholder="09xxxxxxxxx">
+	                      	<input type="number" id="number-input" name="mobile_no" class="form-control form-style" placeholder="09xxxxxxxxx">
 	            		</div>
 	            	</div>
 
@@ -52,28 +97,83 @@
 		                <div class="row">
 		                	<div class="col">
 			                	<label for="bday-input" class="form-label" style="float:left; font-weight: bold; font-size: 1.1vw">Birth Date</label><br>
-			                	<input type="date" id="bday-input" class="form-control form-style" placeholder="Birth Date"><br>
+			                	<input type="date" id="bday-input" name="bday" class="form-control form-style" placeholder="Birth Date"><br>
 			                </div>
 
 			                <div class="col">
 			                	<label for="age-input" class="form-label" style="float:left; font-weight: bold; font-size: 1.1vw">Age</label><br>
-			                	<input type="text" id="age-input" class="form-control form-style" placeholder="" readonly><br>
+			                	<input type="text" id="age-input" name="age" class="form-control form-style" placeholder="Age"><br>
 			                </div>
 		                </div>
 
 		                <label for="address-input" class="form-label" style="float:left; font-weight: bold; font-size: 1.1vw">Address</label><br>
-	                    <textarea class="form-control" id="address-input" rows="5"></textarea><br>
+	                    <textarea class="form-control" name="address" id="address-input" rows="5"></textarea><br>
 
 	                    <label for="form-input" class="form-label" style="float:left; font-weight: bold; font-size: 1.1vw">Valid ID</label>
 	                    <a href="#"><i class="fas fa-question-circle" title="More Information" style="float:left; margin-left:5px; margin-top: 7px"></i></a>
-			            <input type="file" id="form-input" class="form-control form-style">
+			            <input type="file" id="form-input" name="identification" class="form-control form-style">
 		              </div>
 		            </div> 
+
+		             <div class="row g-0">
+	            	<div class="col g-0" style="padding: 2%">
+	            		<div class="container-fluid">
+	            			<label for="user-input" class="form-label" style="float:left; font-weight: bold; font-size: 1.1vw">Username</label><br>
+	                      	<input type="text" id="user-input" name="username" class="form-control form-style" placeholder="Username"><br>
+
+	                      	<label for="password-input" class="form-label" style="float:left; font-weight: bold; font-size: 1.1vw">Password</label><br>
+	                      	<input type="password" name="password" id="password-input" class="form-control form-style" placeholder="Password"><br>
+
+	                      	<label for="password2-input" class="form-label" style="float:left; font-weight: bold; font-size: 1.1vw">Confirm Password</label><br>
+	                      	<input type="password" name="confirm_password" id="password2-input" class="form-control form-style" placeholder="Confirm Password"><br>
+
+	                      	
+	            		</div>
+	            	</div>
+
+	            	<div>
+	            		<div class="container-fluid mb-3">
+		                <div class="row">
+		                	<div class="accordion" id="terms-conditions">
+		                		<div class="accordion-item">
+		                			<h2 class="accordion-header" id="terms-header">
+		                				<button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseOne" aria-expanded="true" aria-controls="panelsStayOpen-collapseOne">
+		                				<strong>Terms & Conditions</strong></button>
+		                			</h2>
+		                			
+		                			<div id="panelsStayOpen-collapseOne" class="accordion-collapse collapse show" aria-labelledby="panelsStayOpen-headingOne">
+								      <div class="accordion-body">
+								        <strong>JNK Loaning Corp. Terms & Conditions</strong><br><br>
+
+								         By accessing this web site, you are agreeing to be bound by these web site Terms and Conditions of Use, all applicable laws and regulations, and agree that you are responsible for compliance with any applicable local laws. If you do not agree with any of these terms, you are prohibited from using or accessing this site. The materials contained in this web site are protected by applicable copyright and trade mark law.
+								      </div>
+								    </div>
+
+								    <div class="form-check" style="float:left; font-weight: bold">
+			                      		<br>
+			                      		<label class="form-check-label" for="terms-input">I Agree to the Terms & Conditions</label>
+			                      		<input class="form-check-input" name="agree" type="checkbox" id="terms-input">
+			              </div>
+		                		</div>
+		                	</div>
+		                </div>
+		              </div>
+	            	</div>
+
+							<div>
+		            	<button type="submit" name="submit" class="btn btn-primary reg-submit btn-block">Submit</button>
+        					<button type="cancel" class="btn btn-danger btn-block">Cancel</button>
+							</div>
+
+	           	</div>
+
 	           	</div>
 	          </div>
 	      	</form>
         </div>
 
+
+<!--
         <div class="container-fluid">
         	<div class="row g-0" style="padding-bottom: 3%">
         		<div class="col g-0">
@@ -88,6 +188,8 @@
         		</div>
         	</div>
         </div>
+
+-->
       </div>
     </div>
 
